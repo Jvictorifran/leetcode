346367b3c84f9ade918c3d4dbb30efb759494e07
Jvictorifran <joaovictorcandidoifran@gmail.com>
2025-07-16 15:49:18 -0400

    remester leetcode15

--- leetcode15.c
+++ leetcode15.c
@@ -38,3 +38,42 @@
 // Sample Output 2
 
 // 1 1 1 2 0 1 2 0 5 0 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char s[999];
+    int v[10], i;
+    scanf("%s", s);
+    for(i = 0; i < 10; i++)
+        v[i] = 0;
+    for(i = 0; s[i] != '\0'; i++ ){
+        if(s[i] == '0'){
+            v[0] += 1;
+        }else if(s[i] == '1'){
+            v[1] += 1; 
+        }else if(s[i] == '2'){
+            v[2] += 1;
+        }else if(s[i] == '3'){
+            v[3] += 1;
+        }else if(s[i] == '4'){
+            v[4] += 1;
+        }else if(s[i] == '5'){
+            v[5] += 1;
+        }else if(s[i] == '6'){
+            v[6] += 1;
+        }else if(s[i] == '7'){
+            v[7] += 1;
+        }else if(s[i] == '8'){
+            v[8] += 1;
+        }else if(s[i] == '9'){
+            v[9] += 1;
+        }
+    }
+    for(int j = 0; j <= 9; j++){
+        printf("%d ", v[j]);
+    }
+    return 0;
+}
